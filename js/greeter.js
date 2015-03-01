f65e6b42a298b4b9bdb1e386833c31ec17cf760c
WokSearedBacon <studiosofinkblot@gmail.com>
2015-03-01 02:45:46 -0600

    Made great progress on wallpaper switching. Added fade in/out transitions, login panel transarency, an active thumb marker. Random wallpaper function is a WIP.

--- js/greeter.js
+++ js/greeter.js
@@ -1,83 +1,105 @@
-
-//Will Revise for new layout
-
-/*
-
-function buildUserList() {
-// User list building
-    var userList = document.getElementById('user-list2');
-    for (var i in lightdm.users) {
-        var user = lightdm.users[i];
-        var tux = 'img/antergos-logo-user.png';
-        var imageSrc = user.image.length > 0 ? user.image : tux;
-        var lastSession = localStorage.getItem(user.name);
-        if (lastSession == null || lastSession == undefined) {
-            localStorage.setItem(user.name, lightdm.default_session);
-            lastSession = localStorage.getItem(user.name);
-        }
-        log('Last Session (' + user.name + '): ' + lastSession);
-        var li = '<a href="#' + user.name + '" class="list-group-item ' + user.name + '" onclick="startAuthentication(\'' + user.name + '\')" session="' + lastSession + '">' +
-            '<img src="' + imageSrc + '" class="img-square" alt="' + user.display_name + '" onerror="imgNotFound(this)"/> ' +
-            '<span>' + user.display_name + '</span>' +
-            '<span class="badge"><i class="fa fa-check"></i></span>' +
-            '</a>';
-        $(userList).append(li);
-    }
-}
-
-*/
-
-
-//Will revise for new layout
-
-/*
-
-function buildSessionList() {
-// Build Session List
-    for (i in lightdm.sessions) {
-        var session = lightdm.sessions[i];
-        var btnGrp = document.getElementById('sessions');
-        var theClass = session.name.replace(/ /g, '');
-        var button = '\n<li><a href="#" id="' + session.key + '" onclick="sessionToggle(this)" class="' + theClass + '">' + session.name + '</a></li>';
-
-        $(btnGrp).append(button);
-    }
-}
-
-*/
+// The Javascripting of the greeter
 
 function get_hostname() {
     var hostname = lightdm.hostname;
     $('#hostname').append("<h3>" + hostname + "</h3>");
 }
 
-//  UI Init
+function update_time() {
+    var time = $("#currentTime");
+    var date = new Date();
+    var twelveHr = [
+        'sq-al',
+        'zh-cn',
+        'zh-tw',
+        'en-au',
+        'en-bz',
+        'en-ca',
+        'en-cb',
+        'en-jm',
+        'en-ng',
+        'en-nz',
+        'en-ph',
+        'en-us',
+        'en-tt',
+        'en-zw',
+        'es-us',
+        'es-mx'];
+    var userLang = window.navigator.language;
+    var is_twelveHr = twelveHr.indexOf(userLang);
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var suffix = "AM";
+    if (hh >= 12) {
+        suffix = "PM";
+        if (is_twelveHr !== -1 && is_twelveHr !== 12) {
+            hh = hh - 12;
+        }
+    }
+    if (mm < 10) {
+        mm = "0" + mm;
+    }
+    if (hh === 0 && is_twelveHr !== -1) {
+        hh = 12;
+    }
+    time.innerHTML = hh + ":" + mm + " " + suffix;
+}
+
+function initialize_timer() {
+    var userLang = window.navigator.language;
+    log(userLang);
+    update_time();
+    setInterval(update_time, 60000);
+}
+
+
+// Interactive UI
 
 $(document).ready(function() {
+    var bgSaved = null;
     
-    //Sets clock?
-    //initialize_timer();
+    //Function for the random background option
+    function randomBG() {
+            localStorage.setItem("bgsaved", '');
+            $(".bgOpt .bg .random").first().addClass('active');
+            var arrayBackground = [];
+            $('.bgOpt').each(function(i) {
+                if (i > 0) {
+                    arrayBackground.push($(this).attr('data-img'));
+                }
+            });
+            var randBG = arrayBackground[Math.floor(Math.random() * arrayBackground.length)];
+            $('#bgDiv').fadeTo('slow', 0.3, function() {
+                $("#bgDiv").css("background", "url('/usr/share/antergos/wallpapers/" + randBG + "')");
+            }).fadeTo('slow', 1);
+    }
+    
+    
+    //Starts clock
+    //initialize_timer();    
     //Acquires hostname
     get_hostname();
     
+    //Builds the list of users
     //buildUserList();
+    //Builds the list of desktops/sessions
     //buildSessionList();
     
     //script for the menuclose button
     $('#menuClose').click(function() {
-        $('#actionsPanel').slideUp(function() {
+        $('#actionsPanel').fadeOut(function() {
             $('#actionsCover').fadeIn();
         });
     });
     
     //Script for wallClose button
     $('#wallClose').click(function() {
-        $('#wallPane').toggle("slide", function(){
-            $('#loginPane').css({opacity: 1});
-            $('#actionsCover').fadeIn();
-        });
+        console.log("Closed!");
+        $('#wallPane').slideUp();
+        $('#loginPane').css("opacity", "1");
+        $('#actionsCover').fadeIn();
     });
-    
+
     //Script for Wallpaper Changer
     $('.bgOpt').click(function() {
         var newBg = $(this).attr("data-img");
@@ -86,19 +108,27 @@ $(document).ready(function() {
         }
 
         else {
-        $('body').css("background-image", "url('/usr/share/antergos/wallpapers/" + newBg + "')"); 
+             $(bgSaved).toggleClass("activeThumb");
+             $(this).toggleClass("activeThumb");
+             $('#bgDiv').fadeTo('slow', .4, function() {
+                        $("#bgDiv").css("background", "url('/usr/share/antergos/wallpapers/" + newBg + "')");
+             }).fadeTo('slow', 1);
+            bgSaved = this;
+            
+            
+            
         }
     });
     
     //Script for Action Menu
     $('#actionsBtn').mouseenter(function() {
         $('#actionsCover').fadeOut(function() {
-            $('#actionsPanel').slideDown();
+            $('#actionsPanel').fadeIn();
             //script for the wallpaper changer
             $('#wallSwitch').click(function() {
-                $('#actionsPanel').slideUp("fast", function() {
-                    $('#loginPane').css({opacity: 0.5});
-                    $('#wallPane').toggle("slide");
+                $('#actionsPanel').fadeOut("fast", function() {
+                    $('#loginPane').css("opacity", "0.5");
+                    $('#wallPane').slideDown();
                 });
             });         
         });
